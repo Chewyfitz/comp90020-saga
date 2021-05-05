83f4a7be8a3490d8631f547008dfb23930cedc0d
Chewyfitz <chewyfitz@gmail.com>
2021-05-06 00:20:12 +1000

    update requirements.txt add dockerfile development env variable implement basic http requests to couchdb

--- payment/run.py
+++ payment/run.py
@@ -1,11 +1,28 @@
 import flask
+import uuid
+import sys
+import requests
 
 app = flask.Flask(__name__)
-# app.config["DEBUG"] = True
+app.config["DEBUG"] = True
 
+def create_accounts():
+    requests.put("http://admin:password@couch:5984/accounts")
 
+# Create Transaction
 @app.route('/', methods=['GET'])
-def home():
-    return "<h1>Sample API response</h1>"
+def test():
+    #uid = uuid.uuid1()
+    #print(f"DEBUG: uid={uid}", file=sys.stderr)
+    db = requests.get("http://admin:password@couch:5984/accounts")
+
+
+    if db.status_code == 404:
+        create_accounts()
+
+
+    print(f"DEBUG: db={db}", file=sys.stderr)
+    print(f"DEBUG: status code={db.status_code}", file=sys.stderr)
+    return {"value": db.json()}
 
 app.run()
