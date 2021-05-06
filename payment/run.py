3147e25a5acbe5d2f6e71717de2ed4229b8f3a0e
Chewyfitz <chewyfitz@gmail.com>
2021-05-06 16:42:48 +1000

    add pydocs for main routes so that communication with this service can be implemented

--- payment/run.py
+++ payment/run.py
@@ -1,9 +1,10 @@
-import flask
 import uuid
 import sys
 import requests
 
-app = flask.Flask(__name__)
+from flask import Flask, request
+
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 def create_accounts():
@@ -19,10 +20,118 @@ def test():
 
     if db.status_code == 404:
         create_accounts()
+        db = requests.get("http://admin:password@couch:5984/accounts")
 
 
     print(f"DEBUG: db={db}", file=sys.stderr)
     print(f"DEBUG: status code={db.status_code}", file=sys.stderr)
     return {"value": db.json()}
+
+@app.route('/create', methods=['POST'])
+def createAccount():
+    """
+    Create an Account
+
+    Usage:
+        POST [host]/create?name="John"&balance=500
+          - send a POST request to /create
+          - include Param "name" to give the account a name
+          - include Param "balance" to provide an initial balance (default 0)
+
+    Returns (success):
+      - ID of the created account
+      - name and balance of the created account
+
+    Returns (failure):
+      - HTTP status code 500
+    """
+    return request.values
+
+@app.route('/deposit/<account>', methods=['PUT'])
+def depositAmt(account):
+    """
+    Deposit an amount to an account
+
+    Usage:
+        PUT [host]/deposit/<account>?amount=<deposit amount>
+          - send a PUT request to /deposit/<account number>
+          - include Param "amount" to provide the amount to deposit
+                eg: PUT localhost:5000/deposit/20?amount=500
+                    withdraws $500 from account 20
+
+    Returns (success):
+      - name, updated balance, and id of the updated account
+      - HTTP status code 200
+
+    Returns (failure):
+      - HTTP status code 500
+    """
+    return {"account": account, "request": request.values}, 200
+
+@app.route('/withdraw/<account>', methods=['PUT'])
+def withdrawAmt(account):
+    """
+    Withdraw an amount to an account
+
+    Usage:
+        PUT [host]/withdraw/<account>?amount=<withdraw amount>
+          - send a PUT request to /withdraw/<account number>
+          - include Param "amount" to provide the amount to withdraw
+                eg: PUT localhost:5000/withdraw/20?amount=500
+                    deposits $500 to account 20
+
+    Returns (success):
+      - name, updated balance, and id of the updated account
+      - HTTP status code 200
+
+    Returns (failure):
+      - HTTP status code 500
+    """
+    return {"account": account, "request": request.values}, 200
+
+@app.route('/transfer/<to_acc>/<from_acc>', methods=['PUT'])
+def transfer(to_acc, from_acc):
+    """
+    Transfer an amount from one account to another
+
+    Usage:
+        PUT [host]/transfer/<deposit_account>/<withdraw_account>?amount=<withdraw amount>
+          - send a PUT request to /transfer/<account number>
+          - include Param "amount" to provide the amount to transfer
+                eg: PUT localhost:5000/transfer/20/5?amount=500
+                    Sends $500 from account 5 to account 20
+
+    Returns (success):
+      - name, updated balance, and id of both updated accounts
+      - HTTP status code 200
+
+    Returns (failure):
+      - HTTP status code 500
+    """
+    amount = request.values['amount']
+    return {"to": to_acc, "from": from_acc, "amt": amount, "request": request.values}, 200
+
+@app.route('/transact', methods=['PUT'])
+def transact():
+    """
+    Withdraw an amount to an account
+
+    This node will double-check to ensure the amounts are correct, and if not re
+
+    Usage:
+        PUT [host]/transact
+          - send a PUT request to /transact with the transaction as params or json data
+
+    Returns (success):
+      - HTTP status code 200
+      - The list of updated account ids, names, and balances
+
+    Returns (failure):
+      - HTTP status code 400 ( if sum(withdraws) != sum(deposits) )
+      - HTTP status code 500
+    """
+    return {"request": request.values}, 200
+
+
 
 app.run()
