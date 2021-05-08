7a2c1d670f5f2ac00992cfd69b1f087c6de549de
saybb <30707324+saybb@users.noreply.github.com>
2021-05-08 13:19:08 +1000

    implement hotels routes

--- hotel/routes/hotels.js
+++ hotel/routes/hotels.js
@@ -7,11 +7,11 @@ axios.defaults.baseURL =
     + (process.env.DATABASE || "localhost")
     + ":5984";
 
-/* GET all flights */
+/* GET all hotels */
 router.get('/', (req, res) => {
   // if we don't have a query
   if (Object.keys(req.query).length === 0) {
-    axios.get("/flights/_all_docs")
+    axios.get("/hotels/_all_docs")
       .then((response) => {
         res.send(response.data);
       }).catch(e => console.log(e));
@@ -20,24 +20,20 @@ router.get('/', (req, res) => {
     var query = {}
     var skip = req.query.skip == null ? 0 : parseInt(req.query.skip);
     var limit = req.query.limit == null ? 25 : parseInt(req.query.limit);
-    if (req.query.flight_num != null) query.flight_num = req.query.flight_num;
-    if (req.query.origin != null) query.origin = req.query.origin;
-    if (req.query.dest != null) query.dest = req.query.dest;
-    if (req.query.departure != null) query.departure = req.query.departure;
-    if (req.query.arrival != null) query.arrival = req.query.arrival;
+    if (req.query.location != null) query.location = req.query.location;
     if (req.query.price != null) query.price = req.query.price;
 
     // send query off
-    axios.post("/flights/_find", data = {"selector": query, "skip": skip, "limit": limit}
+    axios.post("/hotels/_find", data = {"selector": query, "skip": skip, "limit": limit}
       ).then((response) => {
         res.send(response.data.docs);
       }).catch(e => console.log(e));
   }
 });
 
-/* GET flight by id */
+/* GET hotel by id */
 router.get('/:id', (req, res) => {
-  axios.get("/flights/" + req.params.id)
+  axios.get("/hotels/" + req.params.id)
     .then((response) => {
       res.send(response.data);
     }).catch(e => {
